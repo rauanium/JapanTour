ce981d1eb51d3c75ac867c323d0a66f48109c0f7
rauanium <a.spark69@gmail.com>
2021-03-16 21:21:25 +0600

    Initial Commit

---
+++ JapanTour/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JapanTour
+//
+//  Created by CyberZen on 16.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
