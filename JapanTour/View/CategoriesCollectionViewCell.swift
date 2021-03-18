559a9c9a70b18fcbfda0ad95395aa04c33d8af27
rauanium <a.spark69@gmail.com>
2021-03-18 22:59:32 +0600

    tried to finish collection view

---
+++ JapanTour/View/CategoriesCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CategoriesCollectionViewCell.swift
+//  JapanTour
+//
+//  Created by CyberZen on 18.03.2021.
+//
+
+import UIKit
+
+class CategoriesCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    @IBOutlet weak var categoryLabel: UILabel!
+    
+}
--- JapanTour/ViewController.swift
+++ JapanTour/ViewController.swift
@@ -7,15 +7,37 @@
 
 import UIKit
 
+
+var categoriesData = ["🌳 Nature", "🍜 Food", "⛩ Culture"]
+
 class ViewController: UIViewController {
 
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         
-        //helloworls
+        
+        categories.register(UINib(nibName: "CategoriesCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "CategoriesCollectionViewCell")
+        categories.delegate = self
+        categories.dataSource = self
     }
-
+    @IBOutlet weak var categories: UICollectionView!
+    
 
 }
 
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource{
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return categoriesData.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = categories.dequeueReusableCell(withReuseIdentifier: "CategoriesCollectionViewCell", for: indexPath) as! CategoriesCollectionViewCell
+        cell.categoryLabel.text = categoriesData[indexPath.row]
+        print(categoriesData[indexPath.row])
+        return cell
+    }
+    
+}
